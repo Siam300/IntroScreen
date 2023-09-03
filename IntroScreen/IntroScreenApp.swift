7d074b3f22ec18fe11551680f326fbab1257b1cf
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-09-04 01:20:05 +0600

    Initial Commit

---
+++ IntroScreen/IntroScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroScreenApp.swift
+//  IntroScreen
+//
+//  Created by Auto on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct IntroScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
