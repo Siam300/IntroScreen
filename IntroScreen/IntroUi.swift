119c58b3e571bb5be045c4acfc0125673ad60d64
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-09-04 22:31:07 +0600

    Font changed and code optimized

--- IntroScreen/IntroUi.swift
+++ IntroScreen/IntroUi.swift
@@ -8,100 +8,41 @@
 import SwiftUI
 
 struct IntroUi: View {
-    
-    let pages: [OnboardingPage]
-    
+    let pages: [onPage]
     @State private var currentPageIndex = 0
     
     init() {
         let introDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi nulla purus neque quisque dictum dui. Accumsan fames adipiscing."
-        
         self.pages = [
-            OnboardingPage(imageName: "cuate", introduction: "Introduction 1", description: introDescription, imageSize: CGSize(width: 300, height: 200)),
-            OnboardingPage(imageName: "rafiki1", introduction: "Introduction 2", description: introDescription, imageSize: CGSize(width: 300, height: 200)),
-            OnboardingPage(imageName: "bro", introduction: "Introduction 3", description: introDescription, imageSize: CGSize(width: 300, height: 200)),
-            OnboardingPage(imageName: "rafiki", introduction: "Explore the app", description: introDescription, imageSize: CGSize(width: 400, height: 300)),
+            onPage(imageName: "cuate", introduction: "Introduction 1", description: introDescription, imageSize: CGSize(width: 300, height: 200)),
+            onPage(imageName: "rafiki1", introduction: "Introduction 2", description: introDescription, imageSize: CGSize(width: 300, height: 200)),
+            onPage(imageName: "bro", introduction: "Introduction 3", description: introDescription, imageSize: CGSize(width: 300, height: 200)),
+            onPage(imageName: "rafiki", introduction: "Explore the app", description: introDescription, imageSize: CGSize(width: 400, height: 300)),
         ]
     }
     
     var body: some View {
         VStack {
             Spacer()
-            
-            Image(pages[currentPageIndex].imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: pages[currentPageIndex].imageSize.width, height: pages[currentPageIndex].imageSize.height)
-                .padding()
-            
-            Text(pages[currentPageIndex].introduction)
-                .font(Font.custom("Outfit", size: 24).weight(.semibold))
-                .foregroundColor(.black)
-                
-            
-            Text(pages[currentPageIndex].description)
-                .font(Font.custom("Outfit", size: 15))
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color(red: 0.34, green: 0.33, blue: 0.33))
-                .frame(width: 349, height: 74, alignment: .top)
-                
-            
+            contentView(
+                imageName: pages[currentPageIndex].imageName,
+                introduction: pages[currentPageIndex].introduction,
+                description: pages[currentPageIndex].description,
+                imageSize: pages[currentPageIndex].imageSize,
+                pageCount: pages.count,
+                currentPageIndex: $currentPageIndex
+            )
             Spacer()
-            DotIndicator(currentPageIndex: $currentPageIndex, pageCount: pages.count)
+            dotIndicator(currentPageIndex: $currentPageIndex, pageCount: pages.count)
             Spacer()
-            
             if currentPageIndex == pages.count - 1 {
                 Spacer()
-                
-                Button(action: {
-                    
-                }, label: {
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.blue)
-                            .frame(width: 160, height: 51)
-                            .cornerRadius(17)
-                        Text("Get Started")
-                            .font(
-                                Font.custom("Outfit", size: 20)
-                                    .weight(.semibold)
-                            )
-                            .foregroundColor(.white)
-                    }
-                })
+                getStartedButton()
             } else {
                 HStack {
-                    Button(action: {
-                        currentPageIndex = pages.count - 1
-                    }, label: {
-                        Text("Skip")
-                            .font(
-                                Font.custom("Outfit", size: 24)
-                                    .weight(.semibold)
-                            )
-                            .foregroundColor(.black)
-                    })
-                    .padding()
+                    skipButton(currentPageIndex: $currentPageIndex, pageCount: pages.count)
                     Spacer()
-                    
-                    Button(action: {
-                        currentPageIndex += 1
-                    }, label: {
-                        ZStack {
-                            Rectangle()
-                                .foregroundColor(.clear)
-                                .frame(width: 107, height: 51)
-                                .background(Color.blue)
-                                .cornerRadius(17)
-                            Text("Next")
-                                .font(
-                                    Font.custom("Outfit", size: 20)
-                                        .weight(.semibold)
-                                )
-                                .foregroundColor(.white)
-                        }
-                    })
-                    .padding()
+                    nextButton(currentPageIndex: $currentPageIndex, pageCount: pages.count)
                 }
             }
         }
@@ -109,21 +50,20 @@ struct IntroUi: View {
     }
 }
 
-struct OnboardingPage {
+struct onPage {
     let imageName: String
     let introduction: String
     let description: String
     let imageSize: CGSize
 }
 
-struct DotIndicator: View {
+struct dotIndicator: View {
     @Binding var currentPageIndex: Int
     let pageCount: Int
     
     var body: some View {
         HStack {
             Spacer()
-            
             ForEach(0..<pageCount, id: \.self) { index in
                 Circle()
                     .frame(width: 10, height: 10)
@@ -132,7 +72,6 @@ struct DotIndicator: View {
                         currentPageIndex = index
                     }
             }
-            
             Spacer()
         }
         .padding(.vertical, 20)
@@ -145,3 +84,91 @@ struct IntroUi_Previews: PreviewProvider {
     }
 }
 
+struct contentView: View {
+    var imageName: String
+    var introduction: String
+    var description: String
+    var imageSize: CGSize
+    var pageCount: Int  // Added pageCount property
+    @Binding var currentPageIndex: Int
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: imageSize.width, height: imageSize.height)
+            .padding()
+        Text(introduction)
+            .font(Font.custom("Outfit", size: 24).weight(.semibold))
+            .foregroundColor(.black)
+        
+        Text(description)
+            .font(Font.custom("Outfit", size: 15))
+            .multilineTextAlignment(.center)
+            .foregroundColor(Color(red: 0.34, green: 0.33, blue: 0.33))
+            .frame(width: 349, height: 74, alignment: .top)
+    }
+}
+
+struct getStartedButton: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("Get Started")
+                .font(
+                    Font.custom("Outfit", size: 20)
+                        .weight(.semibold)
+                )
+                .foregroundColor(.white)
+        })
+        .frame(width: 160, height: 51)
+        .background(Color.blue)
+        .cornerRadius(17)
+    }
+}
+
+struct skipButton: View {
+    @Binding var currentPageIndex: Int
+    var pageCount: Int
+    
+    var body: some View {
+        Button(action: {
+            currentPageIndex = pageCount - 1
+        }, label: {
+            Text("Skip")
+                .font(
+                    Font.custom("Outfit", size: 24)
+                        .weight(.semibold)
+                )
+                .foregroundColor(.black)
+        })
+        .padding()
+    }
+}
+
+struct nextButton: View {
+    @Binding var currentPageIndex: Int
+    var pageCount: Int
+    
+    var body: some View {
+        Button(action: {
+            currentPageIndex += 1
+        }, label: {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 107, height: 51)
+                    .background(Color.blue)
+                    .cornerRadius(17)
+                Text("Next")
+                    .font(
+                        Font.custom("Outfit", size: 20)
+                            .weight(.semibold)
+                    )
+                    .foregroundColor(.white)
+            }
+        })
+        .padding()
+    }
+}
